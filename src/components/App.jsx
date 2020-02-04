a1a403291042d1cd73e81868c0aec0e90e334084
David Hong <davidhong@Davids-MacBook-Pro-10.local>
2020-02-04 11:05:49 -0500

    Updated folder structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,8 @@
 import React from "react";
-import ExampleContextUser from "./example-context/ExampleContextUser";
+import { Header, BodyWrapper, Footer } from "./layouts";
 
 // Only needs to import CSS once at <App /> level. All imports for styling should happen in main.scss level from here on
 import "./main.scss";
-import Header from "./header/Header";
-import BodyWrapper from "./body-wrapper/BodyWrapper";
-import Footer from "./footer/Footer";
 
 const App = () => (
     <div className="app-container flex flex-column flex-1">
---
+++ src/components/layouts/index.js
@@ -0,0 +1,3 @@
+export { default as BodyWrapper } from "./body-wrapper/BodyWrapper";
+export { default as Footer } from "./footer/Footer";
+export { default as Header } from "./header/Header";

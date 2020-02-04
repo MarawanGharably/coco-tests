93ee33f14c05a104329da02301cb010a59f86c1f
Mike <michael.obsessvr.com>
2020-02-04 18:46:12 -0500

    Created a button storybook component

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,7 @@ module.exports = {
     stories: [
         '../stories/**/*.stories.jsx'
     ],
-    addons: ['@storybook/addon-actions', '@storybook/addon-links'],
+    addons: ['@storybook/addon-actions', '@storybook/addon-knobs', '@storybook/addon-links'],
     webpackFinal: async config => {
         // do mutation to the config
 

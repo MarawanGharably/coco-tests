26cf68f1b1e900b6a882d46b009ca3f61f4a47d2
David <eunpyohong@gmail.com>
2020-02-06 18:07:34 +0000

    Feature/jest tests

--- src/layouts/header/Header.jsx
+++ src/layouts/header/Header.jsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
 import ObsessHeaderLogo from "./obsess-logo/ObsessHeaderLogo";
 import UserIcon from "./user-icon/UserIcon";
 
+// imgUrl passed down just for testing purposes for now...
 const Header = () => (
     <header className="header-container full-width flex">
         <div className="header-left-side flex flex-center">
@@ -12,5 +14,13 @@ const Header = () => (
         </div>
     </header>
 );
+
+Header.defaultProps = {
+    imgUrl: ""
+};
+
+Header.propTypes = {
+    imgUrl: PropTypes.string
+};
 
 export default Header;
--- src/layouts/header/user-icon/UserIcon.jsx
+++ src/layouts/header/user-icon/UserIcon.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
+import PropTypes from "prop-types";
 
-const DEFAULT_USER_ICON_URL = "https://via.placeholder.com/150";
 const DROPDOWN_ICON_URL = "https://cdn.obsessvr.com/dropdown-bar-icon.svg";
 
-const UserIcon = () => {
+const UserIcon = ({ imgUrl }) => {
     const [displayDropdown, setDisplayDropdown] = useState(false);
     const toggleDropdown = () => {
         setDisplayDropdown(!displayDropdown);
@@ -13,13 +13,21 @@ const UserIcon = () => {
     return (
         <button id="user-icon-button" className="flex flex-center" type="button" onClick={toggleDropdown}>
             <div id="user-icon-image-container">
-                <img id="user-icon-image" className="full-width" alt="user icon" src={DEFAULT_USER_ICON_URL} />
+                <img id="user-icon-image" className="full-width" alt="user icon" src={imgUrl} />
             </div>
             <div id="user-icon-dropdown-container">
                 <img id="user-icon-dropdown-icon" className="full-width" alt="dropdown icon" src={DROPDOWN_ICON_URL} />
             </div>
         </button>
     );
+};
+
+UserIcon.propTypes = {
+    imgUrl: PropTypes.string
+};
+
+UserIcon.defaultProps = {
+    imgUrl: "https://placedog.net/150/150"
 };
 
 export default UserIcon;

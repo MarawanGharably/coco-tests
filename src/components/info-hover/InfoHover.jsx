32b7ff81b3f6c0195bd620a9c26b4cc815eb560b
David <eunpyohong@gmail.com>
2020-03-10 15:16:03 +0000

    Feature/info hover

---
+++ src/components/info-hover/InfoHover.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+const InfoHover = ({ title, message }) => {
+    const [showMessage, setShowMessage] = useState(false);
+
+    const handleHover = () => {
+        setShowMessage(true);
+    };
+
+    const handleUnhover = () => {
+        setShowMessage(false);
+    };
+
+    return (
+        <div className="info-hover-container">
+            <div className="info-hover-button-container flex flex-center" onMouseOver={handleHover} onFocus={handleHover} onMouseOut={handleUnhover} onBlur={handleUnhover}>
+                <span className="info-hover-button-icon">i</span>
+            </div>
+            {showMessage && (
+                <div className="info-hover-message-container flex flex-center flex-column">
+                    <header className="info-hover-message-title">{title}</header>
+                    <p className="info-hover-message">{message}</p>
+                </div>
+            )}
+        </div>
+    );
+};
+
+InfoHover.propTypes = {
+    title: PropTypes.string,
+    message: PropTypes.string,
+};
+
+InfoHover.defaultProps = {
+    title: '',
+    message: '',
+};
+
+export default InfoHover;

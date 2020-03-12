711d23586a6baf0169d6d202455b4f4b394ab0a1
G <g@gs-macbook-pro.myfiosgateway.com>
2020-03-12 13:57:20 -0400

    Style fix. Better error handling.

--- src/pages/register-page/CreatePasswordPage.jsx
+++ src/pages/register-page/CreatePasswordPage.jsx
@@ -16,7 +16,7 @@ const CreatePasswordPage = () => {
     const [submitting, setSubmitting] = useState(false);
     const [errorMessage, setErrorMessage] = useState('');
 
-    const [state, _] = useFormDataStore();
+    const [state] = useFormDataStore();
     const history = useHistory();
 
     useEffect(() => {
@@ -31,35 +31,40 @@ const CreatePasswordPage = () => {
     const submitPassword = async () => {
         setSubmitting(true);
         setErrorMessage('');
+        try {
+            const newPassword = state.password;
+            const response = await fetch(SET_PASSWORD_URL, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                credentials: 'include', // need this for cookie to set
+                body: JSON.stringify({
+                    username: email,
+                    oldPassword: password,
+                    newPassword: newPassword
+                })
+            });
 
-        const newPassword = state.password;
-        const response = await fetch(SET_PASSWORD_URL, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            credentials: 'include', // need this for cookie to set
-            body: JSON.stringify({
-                username: email,
-                oldPassword: password,
-                newPassword: newPassword
-            })
-        });
-
-        const statusCode = response.status;
-        if (statusCode === 200) {
-            history.push('/profile');
-        } else if (statusCode === 400) {
-            console.error('Bad request');
-            setErrorMessage('Invalid password, please input a valid password.');
+            const statusCode = response.status;
+            if (statusCode === 200) {
+                history.push('/profile');
+            } else if (statusCode === 400) {
+                console.error('Bad request');
+                setErrorMessage('Invalid password, please input a valid password.');
+                setSubmitting(false);
+            } else if (statusCode === 403) {
+                console.log('Already set password');
+                history.push('/login');
+            } else {
+                console.error(response.statusText);
+                setErrorMessage('Server error, please try again later.')
+                setSubmitting(false);
+            }
+        } catch (error) {
+            console.error(error);
             setSubmitting(false);
-        } else if (statusCode === 403) {
-            console.log('Already set password');
-            history.push('/login');
-        } else {
-            console.error(response.statusText);
-            setErrorMessage('Internal server error, please try again later.')
-            setSubmitting(false);
+            setErrorMessage('Server error, please try again later.');
         }
     }
 

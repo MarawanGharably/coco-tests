59e276559bb50ed6660c6a16107ca9317bfbfe2e
G <g@gs-macbook-pro.myfiosgateway.com>
2020-03-18 12:13:31 -0400

    Remove footer in App and moved into individual pages.

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import { Route, Switch } from 'react-router-dom';
 import { AuthContextProvider } from './auth/Auth';
 import PrivateRoute from './components/route/PrivateRoute';
 import Header from './layouts/header/Header';
-import BodyWrapper from './layouts/body-wrapper/BodyWrapper';
 import { HomePageDataStore } from './data-store/home-page-data-store/HomePageDataStore';
 import HomePage from './pages/home-page/HomePage';
 import CreatePage from './pages/create-page/CreatePage';
@@ -12,7 +11,6 @@ import RegisterPage from './pages/register-page/RegisterPage';
 import { ProfilePage, ContextProvider as ProfileContextProvider } from './pages/profile-page/ProfilePage';
 import ErrorPage from './pages/error-page/ErrorPage';
 import LoginPage from './pages/register-page/LoginPage';
-import Footer from './layouts/footer/Footer';
 import CreatePasswordPage from './pages/register-page/CreatePasswordPage';
 
 // Only needs to import CSS once at <App /> level.
@@ -23,49 +21,46 @@ const App = () => (
     <div className="app-container flex flex-column flex-1">
         <AuthContextProvider>
             <Header />
-            <BodyWrapper>
-                <Switch>
-                    <Route
-                        path="/password"
-                        render={() => <CreatePasswordPage />}
-                    />
-                    <Route
-                        path="/login"
-                        render={() => <LoginPage />}
-                    />
-                    <Route
-                        path="/signup"
-                        render={() => <RegisterPage />}
-                    />
-                    <PrivateRoute
-                        path="/"
-                        exact
-                        render={() => (
-                            <HomePageDataStore>
-                                <HomePage />
-                            </HomePageDataStore>
-                        )}
-                    />
-                    <PrivateRoute
-                        path="/create"
-                        render={() => <CreatePage />}
-                    />
-                    <PrivateRoute
-                        path="/profile"
-                        render={() => (
-                            <ProfileContextProvider>
-                                <ProfilePage />
-                            </ProfileContextProvider>
-                        )}
-                    />
-                    <PrivateRoute
-                        path="/404"
-                        exact
-                        render={() => <ErrorPage statusCode="404" />}
-                    />
-                </Switch>
-            </BodyWrapper>
-            <Footer />
+            <Switch>
+                <Route
+                    path="/password"
+                    render={() => <CreatePasswordPage />}
+                />
+                <Route
+                    path="/login"
+                    render={() => <LoginPage />}
+                />
+                <Route
+                    path="/signup"
+                    render={() => <RegisterPage />}
+                />
+                <PrivateRoute
+                    path="/"
+                    exact
+                    render={() => (
+                        <HomePageDataStore>
+                            <HomePage />
+                        </HomePageDataStore>
+                    )}
+                />
+                <PrivateRoute
+                    path="/create"
+                    render={() => <CreatePage />}
+                />
+                <PrivateRoute
+                    path="/profile"
+                    render={() => (
+                        <ProfileContextProvider>
+                            <ProfilePage />
+                        </ProfileContextProvider>
+                    )}
+                />
+                <PrivateRoute
+                    path="/404"
+                    exact
+                    render={() => <ErrorPage statusCode="404" />}
+                />
+            </Switch>
         </AuthContextProvider>
     </div>
 );

118047900cc65fd28d4161577f654394e2d9620c
Mike <michael.obsessvr.com>
2020-08-11 16:07:47 -0400

    commented out app context provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-import { AuthContextProvider } from './auth/Auth';
+// import { AuthContextProvider } from './auth/Auth';
 import PrivateRoute from './components/route/PrivateRoute';
 import Header from './layouts/header/Header';
 import { HomePageDataStore } from './data-store/home-page-data-store/HomePageDataStore';
@@ -19,49 +19,49 @@ import './main.scss';
 
 const App = () => (
     <div className="app-container flex flex-column flex-1">
-        <AuthContextProvider>
-            <Header />
-            <Switch>
-                <Route
-                    path="/password"
-                    render={() => <CreatePasswordPage />}
-                />
-                <Route
-                    path="/login"
-                    render={() => <LoginPage />}
-                />
-                <Route
-                    path="/signup"
-                    render={() => <RegisterPage />}
-                />
-                <PrivateRoute
-                    path="/"
-                    exact
-                    render={() => (
-                        <HomePageDataStore>
-                            <HomePage />
-                        </HomePageDataStore>
-                    )}
-                />
-                <PrivateRoute
-                    path="/create"
-                    render={() => <CreatePage />}
-                />
-                <PrivateRoute
-                    path="/profile"
-                    render={() => (
-                        <ProfileContextProvider>
-                            <ProfilePage />
-                        </ProfileContextProvider>
-                    )}
-                />
-                <PrivateRoute
-                    path="/404"
-                    exact
-                    render={() => <ErrorPage statusCode="404" />}
-                />
-            </Switch>
-        </AuthContextProvider>
+        {/* <AuthContextProvider> */}
+        <Header />
+        <Switch>
+            <Route
+                path="/password"
+                render={() => <CreatePasswordPage />}
+            />
+            <Route
+                path="/login"
+                render={() => <LoginPage />}
+            />
+            <Route
+                path="/signup"
+                render={() => <RegisterPage />}
+            />
+            <PrivateRoute
+                path="/"
+                exact
+                render={() => (
+                    <HomePageDataStore>
+                        <HomePage />
+                    </HomePageDataStore>
+                )}
+            />
+            <PrivateRoute
+                path="/create"
+                render={() => <CreatePage />}
+            />
+            <PrivateRoute
+                path="/profile"
+                render={() => (
+                    <ProfileContextProvider>
+                        <ProfilePage />
+                    </ProfileContextProvider>
+                )}
+            />
+            <PrivateRoute
+                path="/404"
+                exact
+                render={() => <ErrorPage statusCode="404" />}
+            />
+        </Switch>
+        {/* </AuthContextProvider> */}
     </div>
 );
 

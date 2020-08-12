fd215bac7dd25a130d6e1191ac84fbbf80c31ab3
David <eunpyohong@gmail.com>
2020-08-12 16:59:22 -0400

    Re-enable auth

--- src/components/route/PrivateRoute.jsx
+++ src/components/route/PrivateRoute.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
-// import { useAuth } from '../../auth/Auth';  // eslint-disable-line
+import { useAuth } from '../../auth/Auth';  // eslint-disable-line
 
 function PrivateRoute(props) {
-    // const [state] = useAuth(); // eslint-disable-line
+    const [state] = useAuth(); // eslint-disable-line
     return (
         // eslint-disable-next-line
-        // state.isAuthenticated ? <Route {...props} /> : <Redirect to="/login" />
-        true ? <Route {...props} /> : <Redirect to="/login" /> // eslint-disable-line
+        state.isAuthenticated ? <Route {...props} /> : <Redirect to="/login" />
     );
 }
 

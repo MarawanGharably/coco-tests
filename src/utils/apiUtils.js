4740360b7610ab66a277c403c042cd8d8ece0e2e
noobulater <aaron@obsessvr.com>
2021-04-08 16:44:00 -0400

    Fixed Aliasing issue in Coco

--- src/utils/apiUtils.js
+++ src/utils/apiUtils.js
@@ -1,55 +1,56 @@
-import { URLS } from './urls';
+import { URLS } from './urls'
 
 const handleErrorResponses = (resolve, reject, response) => {
-    const error = response[0];
+    const error = response[0]
     if (error === 'Signature has expired') {
-        window.location.href = '/login';
+        window.location.href = '/login'
     } else {
-        reject(response);
+        reject(response)
     }
-};
+}
 
 
 const handleResponse = (resolve, reject, response) => {
     switch (response.status) {
         case 200: {
-            resolve(response.json());
-            break;
+            resolve(response.json())
+            break
         }
         case 201: {
-            resolve(response.json());
-            break;
+            resolve(response.json())
+            break
         }
         case 401: {
             response.json()
-                .then((responseJson) => handleErrorResponses(resolve, reject, responseJson));
-            break;
+                .then((responseJson) => handleErrorResponses(resolve, reject, responseJson))
+            break
         }
         case 403: {
             // Need to redirect to login
-            break;
+            break
         }
         default: {
-            response.json().then((err) => reject(err));
-            break;
+            response.json().then((err) => reject(err))
+            break
         }
     }
-};
+}
 
 const headers = (storeId = null) => {
     if (storeId) {
         return {
             'Content-Type': 'application/json',
             'ovr-str-id': storeId,
-        };
+            'Access-Control-Allow-Credentials': 'true'
+        }
     }
     // need this for cookie to set
-    return { 'Content-Type': 'application/json' };
-};
+    return { 'Content-Type': 'application/json' }
+}
 
 const requestOptions = {
     credentials: 'include',
-};
+}
 
 const makePOSTRequest = (_url, payload, storeId = null) => new Promise((resolve, reject) => {
     fetch(_url, {
@@ -59,12 +60,12 @@ const makePOSTRequest = (_url, payload, storeId = null) => new Promise((resolve,
         body: JSON.stringify(payload),
     })
         .then((response) => {
-            handleResponse(resolve, reject, response);
+            handleResponse(resolve, reject, response)
         })
         .catch((err) => {
-            reject(err);
-        });
-});
+            reject(err)
+        })
+})
 
 const makeGETRequest = (_url, storeId = null) => new Promise((resolve, reject) => {
     fetch(_url, {
@@ -72,10 +73,10 @@ const makeGETRequest = (_url, storeId = null) => new Promise((resolve, reject) =
         headers: headers(storeId),
         ...requestOptions,
     }).then((response) => {
-        handleResponse(resolve, reject, response);
+        handleResponse(resolve, reject, response)
     })
-        .catch((err) => reject(err));
-});
+        .catch((err) => reject(err))
+})
 
 const makePUTRequest = (_url, payload, storeId = null) => new Promise((resolve, reject) => {
     fetch(_url, {
@@ -84,11 +85,11 @@ const makePUTRequest = (_url, payload, storeId = null) => new Promise((resolve, 
         ...requestOptions,
         body: JSON.stringify(payload),
     }).then((response) => {
-        handleResponse(resolve, reject, response);
+        handleResponse(resolve, reject, response)
     }).catch((err) => {
-        reject(err);
-    });
-});
+        reject(err)
+    })
+})
 
 const makeDELETERequest = (_url, storeId = null) => new Promise((resolve, reject) => {
     fetch(_url, {
@@ -96,33 +97,33 @@ const makeDELETERequest = (_url, storeId = null) => new Promise((resolve, reject
         headers: headers(storeId),
         ...requestOptions,
     }).then((response) => {
-        handleResponse(resolve, reject, response);
+        handleResponse(resolve, reject, response)
     }).catch((err) => {
-        reject(err);
-    });
-});
+        reject(err)
+    })
+})
 
 // ADMIN APIs
-export const apiAdminCreateUser = (payload) => makePOSTRequest(URLS.CREATE_USER_URL, payload);
-export const apiAdminGetAllStorePolicies = () => makeGETRequest(URLS.STORE_ACCESS_POLICIES_API);
-export const apiGetAllCMSStores = () => makeGETRequest(URLS.GET_ALL_CMS_STORES);
-export const apiAdminCreateStorePolicy = (payload) => makePOSTRequest(`${URLS.STORE_ACCESS_POLICIES_API}`, payload);
+export const apiAdminCreateUser = (payload) => makePOSTRequest(URLS.CREATE_USER_URL, payload)
+export const apiAdminGetAllStorePolicies = () => makeGETRequest(URLS.STORE_ACCESS_POLICIES_API)
+export const apiGetAllCMSStores = () => makeGETRequest(URLS.GET_ALL_CMS_STORES)
+export const apiAdminCreateStorePolicy = (payload) => makePOSTRequest(`${URLS.STORE_ACCESS_POLICIES_API}`, payload)
 
 
 
 
 // PROFILE APIs
-export const apiSubmitProfile = (payload) => makePOSTRequest(URLS.PROFILE_URL, payload);
-export const apiGetProfile = () => makeGETRequest(URLS.PROFILE_URL);
+export const apiSubmitProfile = (payload) => makePOSTRequest(URLS.PROFILE_URL, payload)
+export const apiGetProfile = () => makeGETRequest(URLS.PROFILE_URL)
 
 // HOTSPOT_APIs
-export const apiGetHotspotsByType = (type, storeId, sceneId) => makeGETRequest(`${URLS.CMS_HOTSPOT_URL}/${type}/${sceneId}`, storeId);
-export const apiCreateHotspotByType = (type, storeId, payload) => makePOSTRequest(`${URLS.CMS_HOTSPOT_URL}/${type}`, payload, storeId);
-export const apiUpdateHotspotByType = (type, storeId, hotspotId, payload) => makePUTRequest(`${URLS.CMS_HOTSPOT_URL}/${type}/${hotspotId}`, payload, storeId);
-export const apiDeleteHotspotByType = (type, storeId, hotspotId) => makeDELETERequest(`${URLS.CMS_HOTSPOT_URL}/${type}/${hotspotId}`, storeId);
+export const apiGetHotspotsByType = (type, storeId, sceneId) => makeGETRequest(`${URLS.CMS_HOTSPOT_URL}/${type}/${sceneId}`, storeId)
+export const apiCreateHotspotByType = (type, storeId, payload) => makePOSTRequest(`${URLS.CMS_HOTSPOT_URL}/${type}`, payload, storeId)
+export const apiUpdateHotspotByType = (type, storeId, hotspotId, payload) => makePUTRequest(`${URLS.CMS_HOTSPOT_URL}/${type}/${hotspotId}`, payload, storeId)
+export const apiDeleteHotspotByType = (type, storeId, hotspotId) => makeDELETERequest(`${URLS.CMS_HOTSPOT_URL}/${type}/${hotspotId}`, storeId)
 
 // HOMEPAGE APIs
-export const apiGetClientStores = () => makeGETRequest(URLS.GET_ALL_STORES_URL);
+export const apiGetClientStores = () => makeGETRequest(URLS.GET_ALL_STORES_URL)
 
 // PRODCT PLACEMENT PAGE APIs
-export const apiGetAllScenesData = (storeId) => makeGETRequest(URLS.GET_ALL_SCENES_DATA(storeId), storeId); // eslint-disable-line
+export const apiGetAllScenesData = (storeId) => makeGETRequest(URLS.GET_ALL_SCENES_DATA(storeId), storeId) // eslint-disable-line
--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,5 +1,5 @@
-const merge = require('webpack-merge');
-const baseConfig = require('./webpack.base.js');
+const merge = require('webpack-merge')
+const baseConfig = require('./webpack.base.js')
 
 module.exports = merge(baseConfig, {
     devServer: {
@@ -9,6 +9,7 @@ module.exports = merge(baseConfig, {
         host: '0.0.0.0',
         port: 3000,
         open: true,
+        publicPath: "/"
     },
     devtool: 'source-map',
-});
+})

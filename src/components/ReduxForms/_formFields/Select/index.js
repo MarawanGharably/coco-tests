977ce2ba512d0557665bdabd979349167fb40277
natali3 <natali@obsessvr.com>
2021-07-14 14:49:58 -0400

    Select Field fix

--- src/components/ReduxForms/_formFields/Select/index.js
+++ src/components/ReduxForms/_formFields/Select/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Select from 'react-select';
 
-const Select2 = ({ input = {}, label = '', options = [], placeholder='', disabled, meta: { touched, error, warning } }) => {
+const SelectInput = ({ input = {}, label = '', options = [], placeholder='', disabled, meta: { touched, error, warning } }) => {
 
     const onChangeEvent = (data) => {
         const values = data.map(item=>item.value);
@@ -11,4 +11,4 @@ const Select2 = ({ input = {}, label = '', options = [], placeholder='', disable
     return <Select className="select col-12" isMulti placeholder={placeholder} options={options} onChange={onChangeEvent} />;
 };
 
-export default Select2;
+export default SelectInput;

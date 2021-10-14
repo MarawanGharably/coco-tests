27ccc72742413ce48489c1ba41a33c5517767395
natali3 <natali@obsessvr.com>
2021-10-14 13:45:24 -0400

    add params to flat bg path

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -5,3 +5,14 @@ export const getLabelValuePair = (option, value = null) => {
   };
 };
 
+export const formatDate=(date, format) =>{
+  const map = {
+    mm: date.getMonth() + 1,
+    dd: date.getDate(),
+    yy: date.getFullYear().toString().slice(-2),
+    hh: date.getHours().toString(),
+    yyyy: date.getFullYear(),
+  }
+
+  return format.replace(/mm|dd|hh|yy|yyy/gi, matched => map[matched])
+}

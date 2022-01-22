e82781354e2757b43f1da6f00f5c0f60d30d3456
Natali Baik <natali@obsessvr.com>
2022-01-22 00:53:34 +0000

    #4649 FileInput component

--- src/components/ReduxForms/_formFields/FileInput/index.js
+++ src/components/ReduxForms/_formFields/FileInput/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { Form } from 'react-bootstrap';
 import styles from './fileInput.module.scss';
+
 const Icon = 'https://cdn.obsessvr.com/coco/uploadIcon.png';
-
-
 
 /**
  * @param input {object}
@@ -16,41 +15,74 @@ const Icon = 'https://cdn.obsessvr.com/coco/uploadIcon.png';
  * Example:
  * <Field name='favicon' label='Favicon' component={FileInput} accept='.PNG' placeholder='Upload PNG'/>
  */
-export default function FieldFileInput({ input, label, accept, placeholder = 'Upload File', meta: {visited, error, warning}}) {
-    const { value, ...inputData } = input;
-
-    const onChange = (e) => {
-        if (e.target.files[0]) {
-            const file = e.target.files[0];
-            //console.log('onChange', { file, input });
-            input.onChange(file);
-        }
-    };
+export default function FileInput({ input, label, accept, placeholder = 'Upload File', meta: { visited, error, warning }, }) {
 
     return (
         <Form.Group className={`form-input ${styles['cmp']}`}>
-            <Form.Label>{label}</Form.Label>
+            {label && (<Form.Label>{label}</Form.Label>)}
 
-            <input {...inputData} type="file" accept={accept} onChange={onChange} onBlur={e=>e.preventDefault()} />
+            {!input.value ?
+                (<FileUploader
+                    input={input}
+                    placeholder={placeholder}
+                    accept={accept}
+                />)
+                : (<FilePreview input={input} />)
+            }
 
-            <div className={styles['uploadFileWrapper']}>
-                {input?.value?.name || <NoFileSelected placeholder={placeholder} />}
-            </div>
-            {visited  && (error || warning) && (
-                <ul className="form-field-error">
-                    {error && <li className="field-error">{error}</li>}
-                    {warning && <li className="field-warn">{warning}</li>}
+
+            {visited && (error || warning) && (
+                <ul className='form-field-error'>
+                    {error && <li className='field-error'>{error}</li>}
+                    {warning && <li className='field-warn'>{warning}</li>}
                 </ul>
             )}
         </Form.Group>
     );
 }
 
-const NoFileSelected = ({ placeholder }) => {
+const FileUploader = ({ input, placeholder, accept }) => {
+    const { value, ...inputData } = input;
+    const onChange = (e) => {
+        e.preventDefault();
+        if (e.target.files[0]) {
+            const file = e.target.files[0];
+            const reader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.onloadend = (readData) => {
+                input.onChange([
+                    { content: readData.target.result, filename: file.name.split('.')[0], type: file.type },
+                ]);
+            };
+            // console.log('onChange', { file, input });
+        }
+    };
+
     return (
-        <>
+        <div className={styles['fileUploader']}>
+            <input
+                {...inputData}
+                type='file'
+                accept={accept}
+                onChange={onChange}
+                onBlur={(e) => e.preventDefault()}
+            />
             <img className={`${styles['uploadImg']}`} src={Icon} />
             {placeholder}
-        </>
+        </div>
+    );
+};
+
+//TODO: can display img/png files and delete icon
+const FilePreview = ({ input }) => {
+    const deleteValue=()=>{
+        input.onChange(null);
+    }
+    return (
+        <div className={`d-flex ${styles.filePreview}`}>
+            <i className='fas fa-file'></i>
+            <div>{input?.value?.[0].filename || 'File'}</div>
+            <i className="fas fa-trash-alt" onClick={deleteValue}></i>
+        </div>
     );
 };

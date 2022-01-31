7c19b5a557e078ac875ba59b4d8b62849bf71b58
natali3 <natali@obsessvr.com>
2022-01-31 16:07:41 -0500

    new FieldErrorUI

---
+++ src/components/ReduxForms/commonUI/FieldErrorUI/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './fieldError.module.scss';
+
+const FieldErrorUI = ({ meta }) => {
+	const { touched, error, warning } = meta;
+	if(!touched || (!error && !warning)) return false;
+
+	return (<div className={`${styles.cmp} form-field-error`}>
+		{error && <div className={styles['error']}>{error}</div>}
+		{warning && <div className={styles['warn']}>{warning}</div>}
+	</div>);
+};
+
+FieldErrorUI.propTypes = {
+	meta: PropTypes.InstanceOf(PropTypes.Object).isRequired,
+};
+
+export default FieldErrorUI;

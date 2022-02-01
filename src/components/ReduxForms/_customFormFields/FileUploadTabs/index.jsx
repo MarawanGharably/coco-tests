d3d50a66b49641f95609fa2cf839c218eb9f03e1
natali3 <natali@obsessvr.com>
2022-02-01 10:34:03 -0500

    accept only images

--- src/components/ReduxForms/_customFormFields/FileUploadTabs/index.jsx
+++ src/components/ReduxForms/_customFormFields/FileUploadTabs/index.jsx
@@ -18,12 +18,12 @@ export default function FileUploadTabs ({ label = '', type = '' }) {
             {label && (<Form.Label>{label}</Form.Label>)}
             <div className={styles["tabs"]}>
                 <div className={styles["tab-list"]}>
-                    <button type="button" id = {'0'} className={selectedIndex == 0 ? styles["tab-active"] : styles["tab-inactive"]}
+                    <button type="button" id='0' className={selectedIndex == 0 ? styles["tab-active"] : styles["tab-inactive"]}
                         onClick={onClick}
                         aria-selected="true">
                         Upload
                     </button>
-                    <button type="button" id = {'1'} className={selectedIndex == 1 ? styles["tab-active"] : styles["tab-inactive"]}
+                    <button type="button" id='1' className={selectedIndex == 1 ? styles["tab-active"] : styles["tab-inactive"]}
                         onClick={onClick}
                         aria-selected="false">
                         Link
@@ -32,12 +32,12 @@ export default function FileUploadTabs ({ label = '', type = '' }) {
                 <div
                     className={styles["tab-panel"]} 
                     hidden={selectedIndex == 1}>
-                    <Field name={'imageFile'} component={FieldFileInput} placeholder={"Upload " + (type=='image' ? 'Image' : 'Video')} />
+                    <Field name='imageFile' component={FieldFileInput} accept='image/*' placeholder={"Upload " + (type=='image' ? 'Image' : 'Video')} />
                 </div>
                 <div
                     className={styles["tab-panel"]}
                     hidden={selectedIndex == 0}>
-                    <Field name={'imageURL'} placeholder="Paste Link" component={Input}/>
+                    <Field name='imageURL' placeholder="Paste Link" component={Input}/>
                 </div>
             </div>
         </>
